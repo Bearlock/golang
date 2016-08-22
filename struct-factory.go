698619e86eedf14686621f706df101bfd0cbfd86
Erick Diaz <ediaz@spark.net>
2016-08-22 09:01:27 -0600

    Updating structs

--- struct-factory.go
+++ struct-factory.go
@@ -22,12 +22,22 @@ func (mancer Allomancer) stringify() string {
 	return fmt.Sprintf("%s is a %s who burns %s", mancer.Name, mancer.MistingType, mancer.Metals)
 }
 
+func (mancer *Allomancer) updateTypeAndMetals(mistingType, metals string) {
+	mancer.MistingType = mistingType
+	mancer.Metals = metals
+}
+
 func main() {
 	vin := NewAllomancer("Vin", "Mistborn", "All")
 	spook := NewAllomancer("Spook", "Tineye", "Tin")
 	ham := NewAllomancer("Hammond", "Pewterarm", "Pewter")
+	elend := NewAllomancer("Elend", "Nobleman", "Nothing")
 
 	fmt.Println(vin.stringify())
 	fmt.Println(spook.stringify())
 	fmt.Println(ham.stringify())
+	fmt.Println(elend.stringify())
+
+	elend.updateTypeAndMetals("Mistborn", "All")
+	fmt.Println(elend.stringify())
 }
